1164d9f45f8218a8f7b6e87eb7a9d66dea91d8c1
Joseph Peak <55969233+josephrpeak@users.noreply.github.com>
2023-02-19 02:40:55 -0600

    Update binary_multiplication.py

--- binary_multiplication.py
+++ binary_multiplication.py
@@ -1,14 +1,17 @@
 import booths_algorithm
 import columncompression
+import hursonsscheme
 
 while(True):
-  choice = input("\n[1]: Booth's Algorithm (signed)\n[2]: Column Compression (unsigned)\n[3]: Quit\n")
+  choice = input("\n[1]: Booth's Algorithm (signed)\n[2]: Column Compression (unsigned)\n[3]: Hurson's Scheme\n[4]: Quit\n")
   try:
     if(choice == '1'):
-      booths_algorithm.main()
+      booth.main()
     elif(choice == '2'):
       columncompression.main()
     elif(choice == '3'):
+      hursonsscheme.main()
+    elif(choice == '4'):
       break
     else:
       raise ValueError
